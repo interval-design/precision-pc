6f2722084e2681894a9fae9ceb509e2c8b701a6c
Pony <mycnj123@gmail.com>
2017-11-23 11:12:31 +0800

    style:增加variable.scss中的变量

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="itv-layout-default itv-view">
     <nav class="itv-nav">
-      nav
+      <div class="itv-nav-bar">
+        <nuxt-link to="/">
+          <img src="../assets/logo.png" alt="logo">
+        </nuxt-link>
+        <ul>
+          <li>
+            <nuxt-link to="/">首页</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/">技术</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/">产品及服务</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/">关于普瑞森</nuxt-link>
+          </li>
+        </ul>
+      </div>
     </nav>
+
     <nuxt class="itv-container"/>
     <footer class="itv-footer">
       footer
@@ -14,9 +33,17 @@
   @import "../assets/style/variable";
 
   .itv-nav{
-    height: 50px;
+    height: 56px;
     width: 100%;
-    background: $border;
+    background: $white;
+    &-bar{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 1100px;
+      height: 100%;
+      margin: auto;
+    }
   }
 
   .itv-footer{
